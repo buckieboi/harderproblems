47d5c2c68915b86df69e9fe7d1a71d7d8236ce9c
buckie <buckie9777@gmail.com>
2025-07-11 11:50:43 -0500

    worked on bruteforce  231A problem

--- codeforcesproblems/231A/team.cpp
+++ codeforcesproblems/231A/team.cpp
@@ -1,9 +1,20 @@
+/*
+### flashcard template (problem title)
+- tool:
+- big(o):
+- given input parameters:
+- chain of logic:take input.txt, 'n' takes lines and make variables, update count when condition met, return count.
+- how it works:
+- notes and rules: main function, fstream used to take input,
+- entire code blocks used:
+*/
+
 #include <iostream>
 #include <fstream>
 using namespace std;
 
 int main() {
-    ifstream fin("input.txt"); // open input.txt for reading
+    ifstream fin("input.txt"); 
 
     int n, a, b, c, count = 0;
     fin >> n;
